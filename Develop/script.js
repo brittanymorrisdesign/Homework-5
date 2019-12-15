1ef07319ab8d7d8cd84820525e16e387af01eade
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-12-15 14:53:57 -0500

    Set up local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() { //Event Listener in Jquery
+    console.log(this);
+    var value = $(this).siblings(".description").val();
+    var time = $(this).siblings(".hour").text();
+    
+    localStorage.setItem(time, value);
+  }); 
   $("#currentDay").text(moment());
   
   });
