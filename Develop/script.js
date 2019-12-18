d4cd892039c97056a6cb7d26c50373b7cf57dab6
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-12-17 22:56:29 -0500

    Added setColors Function

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
   $(".saveBtn").on("click", function() { //Event Listener in Jquery
     console.log(this);
     var value = $(this).siblings(".description").val();
-    var time = $(this).siblings(".hour").text();
-    
+    var time = $(this).attr("data-time");
+    var hour = moment().hour();
+
     localStorage.setItem(time, value);
   }); 
  
@@ -25,24 +26,32 @@ $(document).ready(function() {
   //Store hours into an array
   var timeArray = [];
   //Loop through hours and push them into an array
-for (var i = 9; i < 18; i++) {
+  for (var i = 9; i < 18; i++) {
     timeArray.push(moment().hour(i).format("h, a"));
-}
-console.log(timeArray)
+  }
+  console.log(timeArray)
 
   // Background colors
-for (var i = 0; i < timeArray.length; i++) {
-    var hour = moment().hour();
-    var currentId = parseInt($(this).attr("id"))
+  function setColors() {
+    let currentHr = moment().format("H");
+    console.log(moment().format("H"));
+      $(".time-block").each(function() {
+    let currentId = ($(this).attr("id"));
 
-    if (currentId < hour) {
+    if (currentHr < currentId ) {
       $(this).addClass("past")
     }
-    else if (currentId > hour) {
+    else if (currentHr > urrentId ) {
       $(this).addClass("future")
     }
-    else if (currentId === hour) {
+    else if (currentHr === currentId ) {
       $(this).addClass("present")
     }
-  }
+  })
+}
+
+
+
+
+setColors();
 });
